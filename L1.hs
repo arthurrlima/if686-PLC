4fe3e17cb524643d683878596b5248cddb1b78a1
Arthur Lima <arl3@cin.ufpe.br>
2023-11-20 15:57:19 -0300

    Update L1.hs --add Q2b
    Q2b done

--- L1.hs
+++ L1.hs
@@ -1,9 +1,21 @@
 --Lista 1 PLC 2023.2
 --Arthur Romaguera Lima - arl3
 
+
+--1:
 mdc :: Int -> Int -> Int
 mdc a b
  | b == 0       = a
- | b > 0        = mdc b (a `mod` b)
+ | b > 0        = mdc b (mod a b)
  | otherwise    = mdc a (-b)
 
+
+--2a:
+--numDiv :: Integral a => a -> a -> a 
+
+
+--2b:
+numDivr :: Int -> Int -> Int
+numDivr a b 
+ | mod a b == 1     = 0
+ | otherwise        = 1 + numDivr (div a b) b
