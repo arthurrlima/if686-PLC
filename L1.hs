ca31397fd17ed9b3806e26511d3217f59efe3b2c
Arthur Lima <arl3@cin.ufpe.br>
2023-11-22 14:38:14 -0300

    [90%] Update L1.hs

--- L1.hs
+++ L1.hs
@@ -1,7 +1,6 @@
 --Lista 1 PLC 2023.2
 --Arthur Romaguera Lima - arl3
 
-
 --1:
 mdc :: Int -> Int -> Int
 mdc a b
@@ -14,8 +13,36 @@ mdc a b
 --numDiv :: Integral a => a -> a -> a 
 
 
+
 --2b:
 numDivr :: Int -> Int -> Int
 numDivr a b 
  | mod a b == 1     = 0
  | otherwise        = 1 + numDivr (div a b) b
+
+
+--3:
+unicos :: [Integer] -> [Integer]
+unicos []            = []
+unicos (x:xs)
+ | elem x (unicos xs) = [ y | y <- (unicos xs), y /= x]
+ | otherwise          = x : (unicos xs)
+
+
+--4:
+halve :: [a] -> ([a],[a])
+halve []            = ([],[])
+halve (x:xs)        = (iPar (x:xs), iImpar (x:xs))
+
+iPar []     = []
+iPar (x:xs) = x: iImpar xs
+
+iImpar []      = []
+iImpar (x:xs)  = iPar xs
+
+
+--5:
+remDiv :: Int -> [a] -> ([a],[a])
+remDiv n x      = (take n x, drop n x)
+
+
